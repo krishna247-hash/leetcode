fb244d61f6cd5f92a8c42790019728fc2941c017
Krushna <krishnakendre389@gmail.com>
2026-08-25 01:27:33 +0530

    Time: 3 ms (14.46%) | Memory: 18.7 MB (96.86%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        int n = indices.size();
+      
        
         string ans = s;
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < s.size(); i++)
         {
             ans[indices[i]] = s[i];
         }
