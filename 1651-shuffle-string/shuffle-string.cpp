7edcf1a31cb91adf5ba72b89008ee788230d9b44
Krushna <krishnakendre389@gmail.com>
2026-08-25 01:26:46 +0530

    Time: 1 ms (20.29%) | Memory: 18.8 MB (76.94%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         int n = indices.size();
-        unordered_map<int,char> mp;
+       
+        string ans = s;
 
-        for(int i = 0; i < n; i++){
-            mp[indices[i]] = s[i];
-        }
-
-        string ans = "";
-
-
-        for(int i = 0 ; i < n; i++){
-            ans +=mp[i];
+        for(int i = 0; i < n; i++)
+        {
+            ans[indices[i]] = s[i];
         }
 
         return ans;
-
     }
 };
