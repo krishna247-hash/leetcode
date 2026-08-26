a621d7b0cf6f82c6901cae2569c5dc342bac9c2d
Krushna <krishnakendre389@gmail.com>
2026-08-26 14:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (16.51%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,69 +1,19 @@
 class Solution {
 public:
-
- int startingIndex(vector<int> &nums, int target){
-            int index = nums.size();
-
-            int low = 0;  int high = nums.size()-1;
-
-            while(low <= high)
-            {
-                int mid = low + (high - low)/2;
-
-                if(nums[mid] >= target){
-                    index = mid;
-
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
-            }
-            return index;
-
-        }
-
-        int endingIndex(vector<int> &nums, int target){
-            int index = nums.size();
-
-            int low = 0;  int high = nums.size()-1;
-
-            while(low <= high)
-            {
-                int mid = low + (high - low)/2;
-
-                if(nums[mid] > target){
-                    index = mid;
-                    
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
-            }
-
-           
-
-            return index;
-
-        }
-
     vector<int> targetIndices(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-
-    
-
         vector<int> ans;
 
-        for(int i = startingIndex(nums,target); i < endingIndex(nums,target); i++)
-        {
-            ans.push_back(i);
-        }
-      return ans;
 
+        for(int i = 0; i < n;i++){
+            if(nums[i] == target)
+            {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
 
     }
 };
