c572031deb257b63ed76813a2417ab146b8db028
Krushna <krishnakendre389@gmail.com>
2026-08-31 22:42:11 +0530

    Time: 4 ms (40.68%) | Memory: 18.1 MB (49.49%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it: nums)
+        {
+            mp[it]++;
+        }
+
+        for(auto it: mp)
+        {
+            if(it.second % 2 != 0) return false;
+        }
+        return true;
+    }
+};
