e2b51f07e56c3cebd80bafdcc8c602bc9bc37bcc
Krushna <krishnakendre389@gmail.com>
2026-09-02 18:42:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.50%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> mp;
+        for(auto it : s)
+        {
+            mp[it]++;
+        }
+
+        for(auto it: t)
+        {
+            if(mp[it] == 0) return it;
+             mp[it]--;
+        }
+
+        return t[0];
+    }
+};
