4a32ce8ebb65631f7cc9ecbd53bc711236ae41ac
Krushna <krishnakendre389@gmail.com>
2026-09-02 18:44:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.18%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int> mp;
-        for(auto it : s)
-        {
-            mp[it]++;
-        }
+       char XOR =0;
+       for(auto it: s)
+       {
+        XOR = XOR ^ it;
+       }
+       for(auto it: t)
+       {
+        XOR = XOR ^ it;
+       }
 
-        for(auto it: t)
-        {
-            if(mp[it] == 0) return it;
-             mp[it]--;
-        }
-
-        return t[0];
+        return XOR;
     }
 };
