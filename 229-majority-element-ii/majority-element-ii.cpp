e2d2579555fb166e1a84705e4aad0679fa6aa432
Krushna <krishnakendre389@gmail.com>
2026-09-07 00:18:24 +0530

    Time: 3 ms (50.98%) | Memory: 23.9 MB (50.40%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        
+        vector<int> ans;
         sort(nums.begin(),nums.end());
-
+        int prev = nums[0];
         int cnt = 1;
+        int k = n / 3;
 
-        int prior = nums[0];
-        vector<int> temp;
-
-        for(int i = 1; i < n; i++){
-            if(prior != nums[i]){
-                if(cnt > n/3){
-                    temp.push_back(prior);
-
+        for(int i = 1; i < n; i++)
+        {
+            if(prev != nums[i])
+            {
+                if(cnt > k)
+                {
+                    ans.push_back(prev);
                 }
-
-                prior = nums[i];
+                prev = nums[i];
                 cnt = 1;
             }
-            else{
+            else
+            {
                 cnt++;
             }
+            
         }
-
-        if (cnt > n / 3) {
-
-            temp.push_back(prior);
-
+        if(cnt > k)
+        {
+            ans.push_back(prev);
         }
-        return temp;
-
+        return ans;
     }
 };
