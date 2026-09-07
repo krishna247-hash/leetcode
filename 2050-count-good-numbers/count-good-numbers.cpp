acf4a3acaabd1ca2ef2c077e10f5aeff36c771ae
Krushna <krishnakendre389@gmail.com>
2026-09-07 16:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.18%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    long long M = 1e9+7;
+
+    int power(int x, long long n)
+    {
+        int ans = 1;
+        while(n > 0)
+        {
+            if(n % 2 != 0)
+            {
+                ans = ((ans % M) * (x % M))%M;
+                n--;
+            }
+            else
+            {
+                x = ((x % M) * (x % M))%M;
+                n = n / 2;
+            }
+        }
+
+        return ans;
+
+    }
+
+    int countGoodNumbers(long long n) {
+        long long even = (n + 1)/2;
+        long long odd = n / 2;
+
+        return ((power(5,even) % M)* (power(4 , odd) % M)) % M;
+    }
+};
