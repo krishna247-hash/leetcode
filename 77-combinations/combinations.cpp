aab9fcab20835de9ea9df3bb1c8d0676d251474d
Krushna <krishnakendre389@gmail.com>
2026-09-09 22:52:59 +0530

    Time: 35 ms (90.24%) | Memory: 62.5 MB (85.24%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -2,40 +2,28 @@ class Solution {
 public:
 
 
-    void combinations(int j, int n, int k, vector<int>& temp, set<vector<int>>& st,vector<int>&mp)
+    void combinations(int j, int n, int k, vector<int>& temp,vector<vector<int>>& ans)
     {
         if(temp.size() == k)
         {
-            // vector<int> k = temp;
-            // sort(k.begin(),k.end());
-            st.insert(temp);
+            ans.push_back(temp);
             return;
         }
 
         for(int i = j; i <= n; i++)
         {
-            if(mp[i] == 0)
-            {
                 temp.push_back(i);
-                mp[i] = 1;
-                combinations(i+1,n,k,temp,st,mp);
+                combinations(i+1,n,k,temp,ans);
                 temp.pop_back();
-                mp[i] = 0;
-            }
         }
     }
 
 
     vector<vector<int>> combine(int n, int k) {
-        vector<int> mp(n+1 , 0);
         vector<int> temp;
-        set<vector<int>> st;
         vector<vector<int>> ans;
-        combinations(1,n,k,temp,st,mp);
-        for(auto it : st)
-        {
-            ans.push_back(it);
-        }
+        combinations(1,n,k,temp,ans);
+
         return ans;
     }
 };
