de52698bbe7ee6ea94372ba57ee9e21e258b61d0
Krushna <krishnakendre389@gmail.com>
2026-09-16 00:13:30 +0530

    Time: 3 ms (63.96%) | Memory: 18 MB (28.88%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -4,22 +4,21 @@ public:
         vector<int> ans;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        vector<int> temp(n+1,0);
-        for(int i = 1; i <= nums.size(); i++)
+        for(int i = 1; i < n; i++)
         {
-            temp[i] = temp[i-1] + nums[i-1];
+            nums[i] = nums[i] + nums[i-1];
         }
 
         for(int i = 0; i < queries.size(); i++)
         {
             int q = queries[i];
-            int tempAns = 0;
-            int low = 0; int high = n;
+            int tempAns = -1;
+            int low = 0; int high = n-1;
             while(low <= high)
             {
                 int mid = low + (high - low) / 2;
 
-                if(temp[mid] <= q)
+                if(nums[mid] <= q)
                 {
                     tempAns = mid;
                     low = mid + 1;
@@ -27,7 +26,7 @@ public:
                 }
                 else high = mid - 1;
             }
-            ans.push_back(tempAns);
+            ans.push_back(tempAns + 1);
         }
         return ans;
     }
